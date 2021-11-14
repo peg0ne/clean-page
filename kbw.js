42997f99c639a933db99e3d2e041b08b7367880e
peg0ne <oliver@justfootballapp.com>
2021-11-14 17:47:19 +0100

    better handling of commands and some extra commands

---
+++ kbw.js
@@ -0,0 +1,102 @@
+var MARKERTAG = "kbwmarker";
+var MARKERID = "kbwmarker-666-69-420";
+var isOpen = false;
+var current = "";
+var alphabet = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z'
+];
+var inputEl = document.getElementById("searchinputbar");
+
+document.onkeydown = function(e) {
+    if (e.key == 'Escape') {
+        showCreator(false);
+        removeMarkers();
+        isOpen = true;
+        toggleOpen();
+    } else if (e.key == 'Tab') {
+        return;
+    } else if (e.key == 's' && entryCreator.style.display == 'none' && document.activeElement != inputEl) {
+        e.preventDefault();
+        inputEl.focus();
+    } else if (e.key == "f") {
+        if (isOpen) {
+            removeMarkers();
+        } else {
+            createMarkers();
+        }
+        toggleOpen();
+    } else if (isOpen) {
+        e.preventDefault();
+        addToCurrent(e.key);
+    }
+};
+
+function getMarkerParent() {
+    var markers = document.getElementsByTagName(MARKERTAG);
+    for (var i = 0; i < markers.length; i++) {
+        if (markers[i].innerText == current) {
+            console.log(markers[i].parentElement);
+            return markers[i].parentElement;
+        }
+    }
+}
+
+function removeMarkers() {
+    current = "";
+    document.getElementById('kbw').innerText = current;
+    var markers = document.getElementsByTagName(MARKERTAG);
+    for (var i = markers.length - 1; i >= 0; i--) {
+        if (markers[i].id == MARKERID) {
+            markers[i].remove();
+        }
+    }
+}
+
+function createMarkers() {
+    var links = document.getElementsByTagName("a");
+    var buttons = document.getElementsByTagName("button");
+    var inputs = document.getElementsByTagName("input");
+    createMarker(links, 0, 'q');
+    createMarker(buttons, 0, 'w');
+    createMarker(inputs, 0, 'e');
+}
+
+function createMarker(l, startIndex, letter) {
+    for (var i = 0; i < l.length; i++) {
+        if (l[i].style.display == "none" || l[i].style.visibility == "hidden")
+            return;
+        var marker = document.createElement(MARKERTAG);
+        marker.style.backgroundColor = "var(--color2)";
+        marker.style.color = "var(--color0)";
+        marker.innerText = `${letter}${alphabet[i + startIndex]}`;
+        marker.id = MARKERID;
+        l[i].appendChild(marker);
+    }
+}
+
+function toggleOpen() {
+    isOpen = !isOpen;
+}
+
+function addToCurrent(v) {
+    if (!v) return;
+    if (v == 'Backspace') {
+        var newcurrent = current.slice(0, -1);
+        current = newcurrent;
+        document.getElementById('kbw').innerText = current;
+    }
+    if (current.length > 1 && v == 'Enter') {
+        try {
+            parent = getMarkerParent();
+            removeMarkers();
+            toggleOpen();
+            current = '';
+            parent.click();
+        } catch {
+            return;
+        }
+    }
+    if (v.length > 1) return;
+    current += v;
+    document.getElementById('kbw').innerText = current;
+}
--- main.js
+++ main.js
@@ -1,21 +1,24 @@
-var template = `<a class="link" href="">{text}</a>`;
+var template = `<a class="link entry" href="">{text}</a>`;
 var dataEntries = localStorage.getItem("clean-page-links") ?
-    JSON.parse(localStorage.getItem("clean-page-links")) : [{ "name": "test", "url": "file:///C:/Users/Olive/git-repos/start-page/index.html" }];
-
+    JSON.parse(localStorage.getItem("clean-page-links")) : [];
+var bgImgUrl = localStorage.getItem("clean-page-img") ?
+    localStorage.getItem("clean-page-img") :
+    'https://external-preview.redd.it/opakdiLcvPEPEZTlN5GtN7EiO10KMXfHNeGXAhn1WwY.jpg?auto=webp&s=fd90024d17899799c9548ef3d651530e0d826331';
+var usernameValue = localStorage.getItem("clean-page-name") ?
+    localStorage.getItem("clean-page-name") :
+    'setname <NAME>'
 
 var entryCreator = document.getElementById("entryCreator");
 var entryNameInput = document.getElementById("nameInput");
 var entryUrlInput = document.getElementById("urlInput");
 var entryHolder = document.getElementById("entryHolder");
 var inputEl = document.getElementById("searchinputbar");
+var usernameText = document.getElementById("usernameText");
+var userImage = document.getElementById("userImg");
 
+search_duck(`setimg ${bgImgUrl}`);
+search_duck(`setname ${usernameValue}`);
 drawDataEntries();
-
-document.onkeydown = (e) => {
-    if (e.key == 'Escape') {
-        showCreator(false)
-    }
-}
 
 inputEl.onkeydown = (e) => {
     if (e.key == 'Enter') {
@@ -24,20 +27,31 @@ inputEl.onkeydown = (e) => {
     }
 }
 
-function search_duck() {
+function search_duck(val) {
     var searchValue = inputEl.value;
-    if (searchValue.startsWith("-e7")) {
+    if (val) searchValue = val;
+    if (searchValue.startsWith('setname')) {
+        var name = searchValue.replace("setname ", '')
+        usernameText.innerText = name;
+        localStorage.setItem('clean-page-name', name);
+    } else if (searchValue.startsWith('setimg')) {
+        var imgUrl = searchValue.replace("setimg ", '');
+        userImage.style.backgroundImage = `url(${imgUrl})`;
+        localStorage.setItem('clean-page-img', imgUrl);
+    } else if (searchValue.startsWith("-e7")) {
         searchValue = searchValue.replace("-e7 ", "");
-        inputEl.value = "";
         window.open("https://epic7x.com/character/" + searchValue, "_Blank");
     } else {
-        inputEl.value = "";
         searchValue = searchValue.replace(/" "/g, "+");
         window.open("https://duckduckgo.com/?q=" + searchValue, "_Blank");
     }
+    inputEl.value = "";
 }
 
 function drawDataEntries() {
+    if (dataEntries.length > 1) {
+        dataEntries.sort(compare);
+    }
     dataEntries.forEach(entry => {
         var el = parser(template.replace('{text}', entry["name"]), "text/html");
         entryHolder.prepend(el);
@@ -45,6 +59,18 @@ function drawDataEntries() {
         el.onclick = (e) => {
             e.preventDefault();
             window.open(el['url'], "_Blank");
+        }
+        el.onkeydown = (e) => {
+            if (e.key == 'Delete') {
+                e.preventDefault();
+                for (var i = 0; i < dataEntries.length; i++) {
+                    if (dataEntries[i].name == e.target.innerText) {
+                        dataEntries.splice(i, 1);
+                        localStorage.setItem('clean-page-links', JSON.stringify(dataEntries));
+                    }
+                }
+                e.target.remove();
+            }
         }
     });
 }
@@ -56,8 +82,50 @@ function showCreator(isActive = true) {
     }
 }
 
+function addEntry() {
+    var entryName = entryNameInput.value;
+    var entryUrl = entryUrlInput.value;
+    entryNameInput.value = '';
+    entryUrlInput.value = '';
+    if (entryName == '' || entryUrl == "") {
+        alert('Entry name cannot be empty');
+        return;
+    }
+    if (!entryUrl.startsWith('file://') && !entryUrl.startsWith('https://')) {
+        alert('Entry url has to be valid');
+        return;
+    }
+    for (var i = 0; i < dataEntries.length; i++) {
+        if (dataEntries[i].name == entryName || dataEntries[i].url == entryUrl) {
+            alert('Entry already exists');
+            entryNameInput = '';
+            entryUrlInput = '';
+            return;
+        }
+    }
+    dataEntries.push({ "name": entryName, "url": entryUrl });
+    localStorage.setItem('clean-page-links', JSON.stringify(dataEntries));
+    var entries = document.getElementsByClassName('entry');
+    for (var i = entries.length - 1; i >= 0; i--) {
+        entries[i].remove();
+    }
+    drawDataEntries();
+}
+
 function parser(htmlString) {
     var el = document.createElement('div');
     el.innerHTML = htmlString;
     return el.firstChild;
+}
+
+function compare(a, b) {
+    if (!a) return 1;
+    if (!b) return -1;
+    if (a['name'] < b['name']) {
+        return 1;
+    }
+    if (a['name'] > b['name']) {
+        return -1;
+    }
+    return 0;
 }
